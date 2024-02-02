429264c8a1ee4f7da4b09a88cb62a79a1ac5791d
Hank Caswell <david.h.caswell@proton.me>
2024-02-02 16:53:51 -0500

    working contacts file

---
+++ contacts.py
@@ -0,0 +1,49 @@
+import csv
+
+class Contacts:
+    def __init__(self):
+        with open('contacts.csv', 'r') as contact_list:
+            csv_reader = csv.reader(contact_list)
+            self._contacts = list(csv_reader)
+
+    def add_contact(self):
+        print("Enter contact details:")
+        name = input("Name:")
+        phone = input("Phone Number:")
+        contact_dict = {'Name': name, 'Phone': phone}
+        self._contacts.append(contact_dict)
+        self._write_to_csv()
+        print('Contact added successfully.')
+
+    def delete_contact(self):
+        print("Enter contact to be deleted:")
+        contact_name = input('Name: ')
+        for contact in self._contacts:
+            if contact['Name'] == contact_name:
+                self._contacts.remove(contact)
+                self._write_to_csv()
+                print('contact delted successfully')
+                break
+    def display_contacts(self):
+        if self._contacts:
+            print("\nContacts:")
+            for contact in self._contacts:
+                print(contact)
+        else:
+            print("\nNo contacts available.")
+
+    def _write_to_csv(self):
+        with open('contacts.csv', 'w', newline= '') as contact_list:
+            csv_writer = csv.writer(contact_list)
+            csv_writer.writerows([contact.values() for contact in self._contacts])
+
+    #This is Tyler's comment
+
+contact_manager = Contacts()
+
+contact_manager.add_contact()         
+
+            
+    
+
+
