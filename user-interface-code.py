76d102a091f132eb5ee4a59aa8047ad0855ef2dd
crystaljobe <crystaljobe29@gmail.com>
2024-02-03 20:40:29 -0700

    Implement Terminal Phone Menu Interface

--- user-interface-code.py
+++ user-interface-code.py
@@ -1,5 +1,3 @@
-import csv
-import os 
 # required imports for rich 
 from rich import print 
 from rich.console import Console
@@ -9,6 +7,7 @@ from rich.prompt import IntPrompt
 
 # create a console class to allow printing and exporting
 console = Console()
+
 
 # create main menu interface
 def main_menu() :
@@ -37,68 +36,6 @@ def contact_manager() :
     console.rule("[b]End Menu[/b]", style="bold blue")
     contact_manager_selection = IntPrompt.ask("[bold][i]What would you like to do?[/i]", choices=["1", "2", "3", "4"], default="4")
 contact_manager()
-
-class Contacts:
-    def __init__(self):
-#Creates a CSV File
-        self._csv_filename = 'contacts.csv'
-        self._initialize_csv()
-
-        with open('contacts.csv', 'r') as contact_list:
-            csv_reader = csv.DictReader(contact_list)
-            self._contacts = list(csv_reader)
-    def _initialize_csv(self):
-        if not os.path.isfile(self._csv_filename):
-            with open(self._csv_filename,'w', newline = '') as contact_list:
-                csv_writer = csv.writer(contact_list)
-                csv_writer.writerow(['Name', 'Phone'])
-#Method to add contacts
-    def add_contact(self):
-        print("Enter contact details:")
-        name = input("Name:")
-        phone = input("Phone Number:")
-        contact_dict = {'Name': name, 'Phone': phone}
-        self._contacts.append(contact_dict)
-        self._write_to_csv()
-        print('Contact added successfully.')
-#Method to Delete Contacts
-    def delete_contact(self):
-        print("Enter contact to be deleted:")
-        contact_name = input('Name: ')
-        for contact in self._contacts:
-            if contact['Name'] == contact_name:
-                self._contacts.remove(contact)
-                self._write_to_csv()
-                print('contact delted successfully')
-                break
-    def display_contacts(self):
-        if self._contacts:
-            # modified code to format output in terminal -CJ
-            console.rule("[bold red]Contact Book 📕")
-            table = Table(show_header=True, header_style="bold blue", show_lines=True)
-            table.add_column("#", style="dim", width=3, justify="center")
-            table.add_column("Name", min_width=20, justify="center")
-            table.add_column("Phone Number", min_width=12, justify="center")
-            
-            for idx, contact in enumerate(self._contacts, start=1):
-                table.add_row(str(idx), f'[cyan]{contact.get("Name")}[/cyan]', f'[green]{contact.get("Phone")}[/green]')
-            console.print(table)
-        else:
-            console.print("[bold red]No contacts to show[/bold red]")
-
-    def _write_to_csv(self):
-        with open('contacts.csv', 'w', newline='') as contact_list:
-            csv_writer = csv.DictWriter(contact_list, fieldnames=['Name', 'Phone'])
-            csv_writer.writeheader()
-
-            for contact in self._contacts:
-                csv_writer.writerow(contact)
-
-
-            
-contact_manager = Contacts()
-contact_manager.add_contact()
-contact_manager.display_contacts()
 
 # create task manager menu interface
 def task_manager() :
